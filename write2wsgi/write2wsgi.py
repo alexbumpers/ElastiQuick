66d640add7f557833684bf40e4a2da7a23f55566
Alex Bumpers <apbumpers@gmail.com>
2017-06-08 22:19:24 -0500

    Test wsgi config write to dummy file.

---
+++ write2wsgi/write2wsgi.py
@@ -0,0 +1,7 @@
+beanstalk_wsgi = """option_settings:
+  aws:elasticbeanstalk:container:python:
+    WSGIPath: ebdjango/wsgi.py"""
+def write2wsgi(wsgi_file):
+    with open(wsgi_file, 'a') as wsgi_config:
+        aws_wsgi = wsgi_config.write(beanstalk_wsgi)
+write2wsgi('wsgi_test.py')
